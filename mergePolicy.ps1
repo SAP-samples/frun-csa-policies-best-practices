8651f35e1aee91e72981d8c5a9109fc0ba199b50
d024870 <rene.muth@sap.com>
2018-08-29 14:53:33 +0200

    Add files via upload

---
+++ mergePolicy.ps1
@@ -0,0 +1,43 @@
+﻿<#
+.SYNOPSIS
+This powershell script merges several FRUN CSA policies into a new policy
+
+.DESCRIPTION
+This powershell script merges several FRUN CSA policies into a new policy. 
+In case you want to merge policies into one new policies this scripts copies all the checks of the provided policies into a single one.
+
+
+.EXAMPLE
+mergePolicy.ps1 -Desc "SNotes of 201701 to 201706" -Id "SNOTES_201701-06" -files $(Get-ChildItem C:\Users\MyUser\Documents\policy\patchday\ABAP*2017-0[1-6]*.xml|sort) -NewPolicy "C:\Users\MyUser\Documents\policy\patchday\test_merge001.xml"
+
+
+.NOTES
+Version 001 / 25.08.2018 / Please email questions to rene.muth@sap.com
+
+.LINK
+http://www.sap.com
+https://www.sap.com/documents/2016/10/c2659de6-907c-0010-82c7-eda71af511fa.html
+
+#>
+param(  [String[]]$Policies,
+        [string]$NewPolicy="test_merge.xml",
+        [String]$Desc="New merged Policy 001",
+        [String]$Id="NewMergedPol0001"
+        )
+
+# start tag: xml header
+$finalXml = "<targetsystem desc=`"$Desc`" id=`"$Id`" multisql=`"Yes`">"
+
+# loop over files and copy content into new policy
+foreach ($file in $Policies) {
+    [xml]$xml = Get-Content $file
+    $finalXml = $finalXml + $xml.targetsystem.InnerXml
+}
+# close tag
+$finalXml += "</targetsystem>"
+
+# write new policy
+([xml]$finalXml).Save("$NewPolicy")
+
+# status message
+Write-Output "Succceded: New merged policy written to: " $NewPolicy
---
+++ printNote.ps1
@@ -0,0 +1,42 @@
+﻿<#
+.SYNOPSIS
+This powershell script prints all check definition for a note within a policy
+
+.DESCRIPTION
+This powershell script prints all check definition for a note within a policy which allows to check the definition without searching through the policy. To find a note the note description is used which supports wildcards.
+
+
+.EXAMPLE
+printNote.ps1 -Policy "C:\Users\MyUser\Documents\patchday\ABAP_snotes_patchday_2017-01.xml" -Note "0002179233"
+.EXAMPLE
+printNote.ps1 -Policy "C:\Users\MyUser\Documents\patchday\ABAP_snotes_patchday_2017-01.xml" -Note "0002179233.*Correct"
+
+
+.NOTES
+Version 001 / 25.08.2018 / Please email questions to rene.muth@sap.com
+
+.LINK
+http://www.sap.com
+https://www.sap.com/documents/2016/10/c2659de6-907c-0010-82c7-eda71af511fa.html
+
+#>
+param ([string]$Policy ="patchday_2016-01.xml", [string]$Note="")
+
+
+ # Create XML document from file
+[xml]$XMLPolicy = (get-content $Policy)
+
+# Get all configstores 
+$CheckItems = $XMLPolicy.GetElementsByTagName("checkitem")
+
+# search the desc of check item and print it in case it matches
+Foreach ($CheckItem in $CheckItems){
+        $match = $CheckItem.desc -match $Note
+        if ($match -eq "True") {
+        # print check item and compliant and non-compliant rule
+        Write-Output $CheckItem
+        Write-Output "Compliant" $CheckItem.compliant.'#text'
+        Write-Output "Noncompliant "$CheckItem.noncompliant.'#text'
+        }
+}
+
